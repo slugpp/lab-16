96fb72bf77b0e21e307a7c18bf0b4ca509cd36d3
fear <corefea@gmail.com>
2023-03-07 01:36:56 +0700

    คอมพรีททท

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,21 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a,int &b,int &c,int &d){
+	int allow[4]={1,1,1,1};
+	int arr[4]={a,b,c,d};
+	int shuffled[4];
+	int i=0;
+	while(i<4){
+		int ran=rand()%4;	
+		if(allow[ran]==1){
+			allow[ran]=0;
+			shuffled[i++]=arr[ran];
+		}
+	}i=0;
+	a=shuffled[i++];
+	b=shuffled[i++];
+	c=shuffled[i++];
+	d=shuffled[i++];
+}
